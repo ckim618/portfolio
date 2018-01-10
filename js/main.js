6aaf88a29931130aaec60d1d79c79cde4edeff62
ckim618 <ckim618@gmail.com>
2018-01-10 12:08:53 -0800

    added linked in as contact

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(document).ready(info);
 function info() {
     var email = 'ckim618@gmail.com';
     var phone = '909-615-3529';
-    
+    var linkedin = 'https://www.linkedin.com/in/ckim618'
+
+    $('.email').text(email).attr('href', 'mailto:' + email); 
+    $('.linkedin').attr('href', linkedin);        
     $('.phoneNum').text(phone);
-    $('.email').text(email).attr('href', 'mailto:' + email);
 }
